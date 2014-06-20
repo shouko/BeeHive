f52ba9f05050e0b0a39d40736bbea55522a1e3fa
Shouko <h999342@gmail.com>
2014-06-21 01:03:21 +0800

    migrating data structure from CSV to JSON, add PHP backend to enhance scalability and peformance

---
+++ data.php
@@ -0,0 +1,19 @@
+<?php
+require('db.config.php');
+
+$dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
+
+$sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid` ORDER by `timestamp` DESC LIMIT 0,3000";
+$sth = $dbh -> prepare($sql);
+
+$sth -> execute();
+$sth -> setFetchMode(PDO::FETCH_ASSOC);
+$result = $sth->fetchAll();
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($result, JSON_PRETTY_PRINT);
+
+$dbh = NULL;
+
+?>
---
+++ db.config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", '');
+define("DB_NAME", '');
+define("DB_USER", '');
+define("DB_PASS", '');
+
+?>
