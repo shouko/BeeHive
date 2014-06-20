771049e591d558ed6c633ec73c681c26b410a496
Shouko <h999342@gmail.com>
2014-06-21 01:20:14 +0800

    Update some comments

--- data.php
+++ data.php
@@ -7,6 +7,7 @@ $dbh = new PDO($dsn, DB_USER, DB_PASS);
 $sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid` ORDER by `timestamp` DESC LIMIT 0,3000";
 $sth = $dbh -> prepare($sql);
 
+// options for requested data might be available in the future, prepared for $sth -> (array('option1','option2'));
 $sth -> execute();
 $sth -> setFetchMode(PDO::FETCH_ASSOC);
 $result = $sth->fetchAll();
