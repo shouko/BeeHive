ff28006bb589f8251dded07e48c418915d9ce8e6
Shouko <h999342@gmail.com>
2014-06-21 01:46:48 +0800

    add database schema description

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ BeeHive is an experiment for visualizing user location data provided by the mobi
 BeeHive reads data asynchronously, and place a marker on specific location on the map for each single row of data, which basically consists of name, gender, birthday, ID, status, and last updated time.
 
 ## Usage
-
+To set up the database, you may refer to **db.schema.sql** for schema, which is similar with the schema of the database used by the mobile app itself.
 
 ## Future Plan
  - Show realtime data
+ - Filtering options
 
 ## Credits
  - Inspired by Denny Huang
---
+++ db.schema.sql
@@ -0,0 +1,28 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE `bb_user_geo_info` (
+  `latitude` double NOT NULL,
+  `longtitude` double NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  PRIMARY KEY (`timestamp`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `bb_user_info` (
+  `aliasInfo_id` bigint(20) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `birthday` int(11) NOT NULL,
+  `signature` varchar(255) NOT NULL,
+  `customized_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `avatar` bigint(20) NOT NULL,
+  `cover` bigint(20) NOT NULL,
+  `extras` int(11) NOT NULL,
+  `relationship` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `updateTime` int(11) NOT NULL,
+  `like` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
