7ceed9aa0b2b0971bfc2adf254e6c10a51a6be83
Shouko <h999342@gmail.com>
2014-06-22 10:44:11 +0800

    Add gender filtering option

--- data.php
+++ data.php
@@ -7,13 +7,20 @@ if ( isset($_GET['limit']) ){
 	$limit = abs($_GET['limit']);
 }
 
+$gender = '%';
+if( isset($_GET['gender']) && $_GET['gender'] != '-1' ){
+		// -1: all, 0: male, 1: female
+		$gender = abs($_GET['gender']);
+}
+
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
 $dbh = new PDO($dsn, DB_USER, DB_PASS);
 
-$sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid` ORDER by `timestamp` DESC LIMIT 0, ?";
+$sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid` AND `bb_user_info`.`gender` LIKE ? ORDER by `timestamp` DESC LIMIT 0, ?";
 $sth = $dbh -> prepare($sql);
 
-$sth -> bindValue(1, $limit, PDO::PARAM_INT);
+$sth -> bindValue(1, $gender);
+$sth -> bindValue(2, $limit, PDO::PARAM_INT);
 $sth -> execute();
 $sth -> setFetchMode(PDO::FETCH_ASSOC);
 $result = $sth->fetchAll();
