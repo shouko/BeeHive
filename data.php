a35684677ecfe1a5b23b564dbb6d5569c018e84b
shouko <h999342@gmail.com>
2015-03-01 04:49:34 +0800

    Update how SQL query are constructed, enhance performance

--- data.php
+++ data.php
@@ -1,32 +1,40 @@
 <?php
 require('db.config.php');
 
+$sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid`";
+$params = array();
+
+if( isset($_GET['uid']) && $_GET['uid'] != '' ){
+	$sql .= " AND `bb_user_info`.`userid` = ?";
+	$params[] = $_GET['uid'];
+}
+
+if( isset($_GET['gender']) && $_GET['gender'] != '-1' ){
+	// -1: all, 0: male, 1: female
+	$sql .= " AND `bb_user_info`.`gender` LIKE ?";
+	$params[] = $_GET['gender'];
+}
+
 // read 100 rows of data for default
 $limit = 100;
 if ( isset($_GET['limit']) ){
 	$limit = abs($_GET['limit']);
-}
-
-$uid = '%';
-if( isset($_GET['uid']) && $_GET['uid'] != '' ){
-	$uid = abs($_GET['uid']);
-}
-
-$gender = '%';
-if( isset($_GET['gender']) && $_GET['gender'] != '-1' ){
-		// -1: all, 0: male, 1: female
-		$gender = abs($_GET['gender']);
+	$sql .= ' ORDER by `timestamp` DESC LIMIT 0, ?';
 }
 
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
 $dbh = new PDO($dsn, DB_USER, DB_PASS);
 
-$sql = "SELECT * FROM `bb_user_info`, `bb_user_geo_info` WHERE `bb_user_geo_info`.`uid` = `bb_user_info`.`userid` AND `bb_user_info`.`gender` LIKE ? AND `bb_user_info`.`userid` LIKE ? ORDER by `timestamp` DESC LIMIT 0, ?";
 $sth = $dbh -> prepare($sql);
 
-$sth -> bindValue(1, $gender);
-$sth -> bindValue(2, $uid);
-$sth -> bindValue(3, $limit, PDO::PARAM_INT);
+$size = count($params);
+
+for($i = 0; $i < $size; $i++){
+	$sth->bindValue($i+1, $params[$i]);
+}
+
+$sth->bindValue($size + 1, $limit, PDO::PARAM_INT);
+
 $sth -> execute();
 $sth -> setFetchMode(PDO::FETCH_ASSOC);
 $result = $sth->fetchAll();
